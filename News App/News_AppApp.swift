9b76ef0eeddea7bc91df96f6dcdaf6b2c2dafd30
Justin747 <justin747@gmail.com>
2021-08-28 14:41:53 -0700

    Initial Commit

---
+++ News App/News_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_AppApp.swift
+//  News App
+//
+//  Created by Justin747 on 8/28/21.
+//
+
+import SwiftUI
+
+@main
+struct News_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
