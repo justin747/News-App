b5d3a0d956cff44a19e6d8554f3e4787c1e2f964
Justin747 <justin747@gmail.com>
2021-08-29 21:03:23 -0700

    Formatting
    - Added View folder for MVVM
    
    - Replace ContentView with HomeView

---
+++ News App/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  HomeView
+//
+//  Created by Justin747 on 8/29/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
