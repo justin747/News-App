8eeb596e37821a2be419ee65aba74f2f37ea356a
Justin747 <justin747@gmail.com>
2021-10-12 20:27:28 -0700

    Setup API Endpoint
    Setup API Endpoint to handle URL requests

---
+++ News App/EndPoint/NewsEndpoint.swift
@@ -0,0 +1,39 @@
+//
+//  NewsEndpoint.swift
+//  News App
+//
+//  Created by Justin747 on 10/12/21.
+//
+
+import Foundation
+
+protocol APIBuilder {
+    var urlRequest: URLRequest { get }
+    var baseUrl: URL { get }
+    var path: String { get }
+}
+
+enum NewsAPI {
+    case getNews
+}
+
+
+extension NewsAPI : APIBuilder {
+    
+    var baseUrl: URL {
+        switch self {
+        case .getNews:
+            return URL(string: "https://api.lil.software")!
+        }
+    }
+    
+    var path: String {
+        return "/news"
+    }
+    
+    var urlRequest: URLRequest {
+        return URLRequest(url: self.baseUrl.appendingPathComponent(self.path))
+    }
+    
+    
+}
---
+++ News App/Model/NewsResponse.swift
@@ -0,0 +1,29 @@
+//
+//  NewsResponse.swift
+//  News App
+//
+//  Created by Justin747 on 10/12/21.
+//
+
+import Foundation
+
+// MARK: - NewsResponse
+struct NewsResponse: Codable {
+    let articles: [Article]
+}
+
+// MARK: - Article
+struct Article: Codable, Identifiable {
+    let id = UUID()
+    let author: String?
+    let url: String
+    let source, title, articleDescription: String
+    let image: String?
+    let date: Date
+
+    enum CodingKeys: String, CodingKey {
+        case author, url, source, title
+        case articleDescription = "description"
+        case image, date
+    }
+}
