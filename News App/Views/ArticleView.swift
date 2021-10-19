6a1bef96ee00d6c5708f0a8940ddbe79b62a69fd
Justin747 <justin747@gmail.com>
2021-10-18 20:44:22 -0700

    Installed URLImage package
    Installed URLImage package

---
+++ News App/Views/ArticleView.swift
@@ -0,0 +1,21 @@
+//
+//  ArticleView.swift
+//  News App
+//
+//  Created by Justin747 on 10/17/21.
+//
+
+import SwiftUI
+import URLImage
+
+struct ArticleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArticleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArticleView()
+    }
+}
